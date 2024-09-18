08c09df01d2299f87c9ebee6cfce3357297847ac
crio-gunjan <gunjan@crio-users.in>
2024-09-19 01:00:48 +0530

    Update component to export method

--- client/src/components/AppMenu/AppMenu.tsx
+++ client/src/components/AppMenu/AppMenu.tsx
@@ -5,20 +5,22 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  useDisclosure,
 } from "@chakra-ui/react";
 
 type OwnProps = {
   anchor: () => React.ReactElement;
-  children: React.ReactElement;
+  children: (onClose?: () => void) => React.ReactElement;
 };
 
 const AppMenu: React.FC<OwnProps> = ({ anchor, children }) => {
+  const { isOpen, onClose, onOpen } = useDisclosure()
   return (
-    <Menu>
+    <Menu isOpen={isOpen} onOpen={onOpen} onClose={onClose}>
       <MenuButton as={Box} cursor="pointer">
         {anchor()}
       </MenuButton>
-      <MenuList zIndex={1001}>{children}</MenuList>
+      <MenuList zIndex={1001}>{children(onClose)}</MenuList>
     </Menu>
   );
 };
