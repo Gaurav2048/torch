6ee5595127dd1f3419ea42d378ae112f54980a77
Gunjan Kalita <gunjankalita@Gunjans-MacBook-Pro.local>
2024-12-30 14:23:55 +0530

    Add config

--- client/src/hooks/axiosInstance.tsx
+++ client/src/hooks/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://torchapp-gold.vercel.app/v1", // Replace with your API base URL
+  baseURL: "https://torch-20gw8glvb-gaurav2048s-projects.vercel.app//v1", // Replace with your API base URL
   timeout: 1000,
   headers: { "Content-Type": "application/json" },
 });
