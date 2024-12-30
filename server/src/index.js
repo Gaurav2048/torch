287681d0866506dbfb1bdb0e164870e8803cf8ea
Gunjan Kalita <gunjankalita@Gunjans-MacBook-Pro.local>
2024-12-30 13:29:57 +0530

    Update env path

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
 const mongoose = require("mongoose");
 const { server, io } = require("./app");
 const { toogleAvailability } = require("./services/auth.service");
 const Org = require("./models/org.model");
+const path = require("path");
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
 
 const mongooseOptions = {
   useUnifiedTopology: true,
