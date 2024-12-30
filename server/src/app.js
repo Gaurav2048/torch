38b0d5b238a17f6fbcaf1ce236b0b083bc1edd7f
Gunjan Kalita <gunjankalita@Gunjans-MacBook-Pro.local>
2024-12-30 14:04:31 +0530

    update config

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ app.use("/v1", routerV1);
 
 app.use(errorHandler);
 
-const buildPath = path.join(__dirname, '../../client/public');
+const buildPath = path.join(__dirname, '../../client/env');
 app.use(express.static(buildPath));
 
 app.get('*', (req, res) => {
