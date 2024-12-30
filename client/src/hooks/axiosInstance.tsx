1b02d55de402b2669bb9a2b7453ddef0697ea10a
Gunjan Kalita <gunjankalita@Gunjans-MacBook-Pro.local>
2024-12-30 13:39:03 +0530

    Update server url for prod

--- client/src/hooks/axiosInstance.tsx
+++ client/src/hooks/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/v1", // Replace with your API base URL
+  baseURL: "https://torchapp-gold.vercel.app/v1", // Replace with your API base URL
   timeout: 1000,
   headers: { "Content-Type": "application/json" },
 });
